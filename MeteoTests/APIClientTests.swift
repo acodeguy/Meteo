aee49e8af6ad24410ef30fc26a20da5404b43bf4
Andrew <24258342+acodeguy@users.noreply.github.com>
2020-06-23 11:25:02 +0100

    Correct uses of API client object from stub to actual

--- MeteoTests/APIClientTests.swift
+++ MeteoTests/APIClientTests.swift
@@ -2,22 +2,31 @@ import XCTest
 @testable import Meteo
 
 class APIClientTests: XCTestCase {
+    private var urlSession: URLSessionMock!
+    private var apiClientService: APIClient!
+    
+    override func setUp() {
+        urlSession = URLSessionMock()
+        apiClientService = APIClient(session: urlSession)
+    }
+    
+    override func tearDown() {
+        urlSession = nil
+        apiClientService = nil
+    }
+    
     func testCallsTheCorrectURL() {
-        let session = URLSessionMock()
-        let service = APIClientStub(session: session)
         let validURL = URL(string: "\(Constants.API.baseURL)/location/721943")!
         
-        service.fetch(dataType: WeatherResponse.self, from: validURL) { _ in
-            XCTAssertEqual(session.lastURLRequested, validURL)
+        apiClientService.fetch(dataType: WeatherResponse.self, from: validURL) { _ in
+            XCTAssertEqual(self.urlSession.lastURLRequested, validURL)
         }
     }
     
     func testReturnsTheLocationTitle() {
-        let session = URLSessionMock()
-        let service = APIClientStub(session: session)
         let dummyURL = URL(string: "dummy-url")!
         
-        service.fetch(dataType: WeatherResponse.self, from: dummyURL) { result in
+        apiClientService.fetch(dataType: WeatherResponse.self, from: dummyURL) { result in
             switch result {
             case .success(let response):
                 XCTAssertEqual(response.title, "Rome")
@@ -28,11 +37,9 @@ class APIClientTests: XCTestCase {
     }
     
     func testReturnsTheCurrentTemperature() {
-        let session = URLSessionMock()
-        let service = APIClientStub(session: session)
         let dummyURL = URL(string: "dummy-url")!
         
-        service.fetch(dataType: WeatherResponse.self, from: dummyURL) { result in
+        apiClientService.fetch(dataType: WeatherResponse.self, from: dummyURL) { result in
             switch result {
             case .success(let response):
                 if let currentWeather = response.weather.first {
